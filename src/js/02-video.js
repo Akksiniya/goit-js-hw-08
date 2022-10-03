fe18fbdbc5930e8d0c11dd4a973aaf7bfdf1108d
akksinya <akksin.ya999@gmail.com>
2022-10-03 21:03:00 +0300

    add player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const TIME_KEY = 'videoplayer-current-time';
+
+const iframeRef = document.querySelector('#vimeo-player');
+
+const player = new Player(iframeRef);
+
+function saveTime (data) {
+
+    localStorage.setItem(TIME_KEY, data.seconds);
+    
+};
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+let loadingTime = localStorage.getItem(TIME_KEY);
+
+if (loadingTime) {
+    player.setCurrentTime(loadingTime);
+};
+
+
+
+
 
